7405be665a7b91eb73f3927f1fc41f16910b46f2
kumar thapa <kumarthapa@users.noreply.github.com>
2021-05-25 01:26:44 +0530

    25 may 2021
    add tex with spread operator

--- src/component/pages/Shop.js
+++ src/component/pages/Shop.js
@@ -5,24 +5,24 @@ class Shop extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: ''
+      name: [],
+      Newname:[]
     }
-    this.onClickHandler = this.onClickHandler.bind(this);
   }
   onchangeHandler = (e) => {
     e.preventDefault()
     this.setState({ name: e.target.value })
   }
-
   onClickHandler = (e) => {
     e.preventDefault();
     if (this.state.name == '') {
       alert('Please write some rext..!');
     } else {
       this.props.addTotextHandler(this.state.name)
+      this.setState({Newname: [...this.state.Newname,this.props.username.username]})
     }
-
   }
+
   render() {
     return (
       <>
@@ -31,17 +31,18 @@ class Shop extends Component {
             <h2 style={{ margin: '10px 0', padding: '10px 0' }}>Please click button and change your text</h2>
             <input type="text" name="name" onChange={this.onchangeHandler} style={{ margin: '10px 0', padding: '10px 7' }} value={this.state.name} />
 
-            <button onClick={this.onClickHandler} style={{ padding: '16px 20px', backgroundColor: 'hotpink', color: 'white', borderRadius: '7px', border: 'none', fontSize: '17px' }}>Change Text</button>
+            <button onClick={this.onClickHandler} style={{ padding: '16px 20px', backgroundColor: 'hotpink', color: 'white', borderRadius: '7px', border: 'none', fontSize: '17px' }}>Add Text</button>
 
           </div>
           <div style={{ padding: '20px', color: 'white', backgroundColor: 'cornflowerblue', borderRadius: '5px', margin: 'auto 10px', height: '250px' }}>
-            <h3 style={{ margin: '10px 0', padding: '10px 0' }}>{this.props.username.username}</h3>
+          <h3 style={{ margin: '10px 0', padding: '10px 0',display:'flex', flexDirection:'column'}}>{this.state.Newname.map((uName,index)=>{return <p key={index}>{uName}</p>})}</h3>
           </div>
         </div>
       </>
     );
   }
 }
+
 const mapStateToProps = (state) => ({
   username: state.cardItems
 });
--- src/services/reducers/reducer.js
+++ src/services/reducers/reducer.js
@@ -1,8 +1,9 @@
 //'cardItems' my parant is services/reducers/index.js
 import {ADD_TO_TEXT} from "../constants";
 const initialState = {
-  username:''
+  username:[],
 };
+
 export default function cardItems(state = initialState, action) {
   switch (action.type) {
       case ADD_TO_TEXT:
