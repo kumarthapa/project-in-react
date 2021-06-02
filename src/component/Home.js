2d4582a17fcd94cd622009e70a665c81cb2014e6
kumar thapa <kumarthapa@users.noreply.github.com>
2021-06-02 10:38:53 +0530

    2 jun 2021
    Fetched api data

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,79 +1,56 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState} from 'react'
 
+import Shop from './pages/Shop'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import About from './pages/About'
 const Home = () => {
-    const [count, setCount] = useState(1)
-    const [slider, setSlider] = useState('FIRST SLIDER')
+    const [menu, setMenu] = useState ('')
 
-    const onClicksilder = () => {
-        setCount((data) => {
-            return data = count + 1
-        })
-        if (count == 1) {
-            setSlider('FIRST SLIDER')
-        } else if (count == 2) {
-            setSlider('SECOND SLIDER')
-        } else if (count == 3) {
-            setSlider('THIRD SLIDER')
+    const onClikhandler = name => Event =>{
+        Event.preventDefault()
+        setMenu(name)
         }
-        if (count == 3) {
-            setCount(3)
-        }
-    }
-    const onClickprivsilder = () => {
-        setCount((data) => {
-            return data = count - 1
-        })
-        if (count == 1) {
-            setSlider('FIRST SLIDER')
-        } else if (count == 2) {
-            setSlider('SECOND SLIDER')
-        } else if (count == 3) {
-            setSlider('THIRD SLIDER')
-        }
-        if (count == 1) {
-            setCount(1)
-        }
-    }
-
-
+   
     return (
-        <>
-            <div style={{
-                display: 'block', color: '#fff', textAlign: 'center',
-                backgroundColor: 'black', width: '100%'
-            }}>
-                <div style={{
-                    color: '#fff', textAlign: 'center', padding: '180px',
-                    backgroundColor: 'black', width: '100%'
-                }}>
-                    <h1>{slider}</h1>
-                    <h3 style={{ padding: '10px' }}>This is your title</h3>
-                    <p style={{ textAlign: 'center', width: '500px', margin: '20px auto' }}>Contrary to popular belief, Lorem Ipsum is
-                    not simply random text. It has roots
-                    in a piece of classical Latin literature
-                    from 45 BC, making it over 2000 years old.
-                    Richard McClintock, a Latin professor at
-                    Hampden-Sydney College in Virginia </p>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center', margin: '5px' }}>
-                    <button style={{ margin: '5px', padding: '2px 5px', backgroundColor: 'yellow', border: 'none' }}></button>
-                    <button style={{ margin: '5px', padding: '2px 5px', backgroundColor: '#fff', border: 'none' }}></button>
-                    <button style={{ margin: '5px', padding: '2px 5px', backgroundColor: '#fff', border: 'none' }}></button>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center', margin: '30px auto' }}>
-
-                    <button onClick={onClickprivsilder} style={{
-                        padding: '10px 10px', margin: '10px 5px',
-                        border: '1px solid white', borderRadius: '4px', color: '#fff', width: '100px', backgroundColor: 'black'
-                    }}>PRIV</button>
-                    <button onClick={onClicksilder} style={{
-                        padding: '10px 10px', margin: '10px 5px', border: 'none',
-                        borderRadius: '4px', color: 'black', width: '100px'
-                    }}>NEXT</button>
-                </div>
+        <div>
+            <div style={{height:'740vh', width:'20%',padding:'10px', backgroundColor:'darkslateblue',float:'left',color:'#fff'}}>
+                <h2 style={{margin:'10px 10px'}}>Our products</h2>
+                <ul style={{margin:'20px auto'}}>
+                    <li>
+                        <p style={{padding:'10px',cursor:'pointer', color:menu=='shop'?'gray':null}} 
+                        name="shop" onClick={onClikhandler('shop')}>Shop</p>
+                    </li>
+                    <li>
+                        <p style={{padding:'10px',cursor:'pointer', color:menu=='services'?'gray':null}} 
+                        name="services" onClick={onClikhandler('services')}>Services</p>
+                    </li>
+                    <li>
+                        <p style={{padding:'10px',cursor:'pointer', color:menu=='about'?'gray':null}} 
+                        name="about" onClick={onClikhandler('about')}>About us</p>
+                    </li>
+                    <li>
+                        <p style={{padding:'10px',cursor:'pointer', color:menu=='contactus'?'gray':null}} 
+                        name="contactus" onClick={onClikhandler('contactus')}>Contact us</p>
+                    </li>
+                </ul>
+            </div>
+            <div style={{float: 'right', width:'80%', height:'700px'}}>
+            {
+                 menu=='shop'?<div><Shop/></div>:null
+            }
+            {
+                 menu=='services'?<div><Services/></div>:null
+            }
+            {
+                 menu=='about'?<div><About/></div>:null
+            }
+            {
+                 menu=='contactus'?<div><Contact/></div>:null
+            }
             </div>
 
-        </>
+        </div>
     )
 }
 
--- src/component/pages/Shop.js
+++ src/component/pages/Shop.js
@@ -1,27 +1,38 @@
-import React, { Component } from 'react'
-export default class Shop extends Component {
-  render() {
-    
-    return (
-            <>
-            <div style={{width:'700px', margin:'0 auto', padding:'60px', color:'white'}}>
-              <form style={{width:'100%',padding:'70px 70px', backgroundColor:"#000000d4"}}>
-                <label>Name:
-                </label>
-                <input type="text"  name="name" placeholder='name'/>
+import React,{useState,useEffect} from 'react'
+import axios from "axios"
+const Shop = () => {
+const [userdata, setUserdata] = useState([])
 
-                <label>Email:</label>
-                <input type="text"  name="email" placeholder='name'/>
+useEffect(() => {
+axios.get("https://jsonplaceholder.typicode.com/posts")
+.then((response)=>{
+  setUserdata(response.data)
+  console.log(response.data)
+}).catch((err)=>{
+  console.log(err)
+})
 
-                <label>Phone: </label>
-                <input type="text"  name="phone" placeholder='name'/>
-
-                <input type="submit" value='SEND'/>
-              </form>
-            </div>
-            </>      
-            
-          
-    );
-  }
+}, [])
+  return (
+    <>
+    <div><h2 style={{padding:'30px'}}>Fetching data from API</h2><hr/></div>
+   <div style={{display:"flex", flexWrap:'wrap', justifyContent:'center', margin:'20px 0'}}>
+      
+      {
+        userdata.map((userData)=>{
+         return <div style={{borderRadius:'5px', boxShadow:'1px 1px 6px 0px', width:'300px', padding:'15px', margin:'6px', fontWeight:'100'}}>
+                  
+                  <p>Id: {userData.id}</p>
+                  <p>Title: {userData.title}</p>
+                  <p>Body: {userData.body}</p>
+                   
+                  </div>
+        })
+      }
+    </div>
+    </>
+ 
+  )
 }
+
+export default Shop
